4aeb4513c469b88120e9831f7f492cd1e7e804ff
Ryan <ryan.cutter123@yahoo.co.uk>
2018-06-12 14:57:13 +0100

    Update catfind.py

--- catfind.py
+++ catfind.py
@@ -6,4 +6,4 @@ def ctsrch(RA, DEC):
  client = WSDB(database='wsdb', host="goto-observatory.org", user="goto", password="A PASSWORD")
 
 #print(client.catalogues)
- return(client.radial_query(catalogue_name="apassdr9_main", ra=RA, dec=DEC, radius=0.05)
+ return(client.radial_query(catalogue_name="apassdr9_main", ra=RA, dec=DEC, radius=0.05))
