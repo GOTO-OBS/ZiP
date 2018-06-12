3a9b614b075febd472f626a7b019e9f378577bfa
Ryan <ryan.cutter123@yahoo.co.uk>
2018-06-12 14:53:06 +0100

    Update catfind.py

--- catfind.py
+++ catfind.py
@@ -3,7 +3,7 @@ from wsdb import WSDB
 from astropy.table import Table
 
 def ctsrch(RA, DEC):
- client = WSDB(database='wsdb', host="goto-observatory.org", user="goto", password="H4iidNHr")
+ client = WSDB(database='wsdb', host="goto-observatory.org", user="goto", password="A PASSWORD")
 
 #print(client.catalogues)
  return(client.radial_query(catalogue_name="apassdr9_main", ra=RA, dec=DEC, radius=0.05))
