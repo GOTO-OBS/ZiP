8e38fd37737faaf3870ac9fb703d0a07373045f8
Ryan <22173649+ryanc123@users.noreply.github.com>
2019-02-25 11:25:04 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='zogyp',
+      version='1.4.0',
+      description='A parallel version of ZOGY image subtraction for astronomy, with Proper Co-Addition and Image alignment',
+      url='https://github.com/ryanc123/RyZiP',
+      author='Ryan Cutter',
+      author_email='R.Cutter@wawrcik.ac.uk',
+      license='MIT',
+      packages=['zogyp'],
+      package_data={'zogyp': ['configfls/*.txt']},
+      include_package_data=True,
+      zip_safe=False)
