c67001b8338fef98b8074597550a65911cf8826f
Ry <22173649+Ry-C123@users.noreply.github.com>
2020-01-15 12:03:09 +0000

    Added executable

--- setup.py
+++ setup.py
@@ -3,11 +3,16 @@ from setuptools import setup
 setup(name='zogyp',
       version='1.4.5',
       description='A parallel version of ZOGY image subtraction for astronomy, with Proper Co-Addition and Image alignment',
-      url='https://github.com/ryanc123/RyZiP',
+      url='https://zogy-in-parallel.readthedocs.io/en/latest/index.html',
       author='Ryan Cutter',
-      author_email='R.Cutter@wawrcik.ac.uk',
+      author_email='R.Cutter@warwick.ac.uk',
       license='MIT',
       packages=['zogyp'],
       package_data={'zogyp': ['configfls/*.txt']},
       include_package_data=True,
+      entry_points={
+        'console_scripts': [
+            'ZOGY=ZOGY:run'
+        ]
+      }
       zip_safe=False)
